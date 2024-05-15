d4048c218166b0abdac19fcdf89baf122c71c1c4
Mostafa Zewail77 <mostafazewail77@gmail.com>
2024-05-15 23:43:56 +0300

    Implemented MongoDB

--- server/db.js
+++ server/db.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+require('dotenv').config();
+// const mongoURI =  "mongodb://root:MjA1MjItbW9zdGFm@127.0.0.1:27017";
 
+const mongoURI = process.env.MONGO_URI;
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
     const count = retryCount ?? 0;
     try {
-        await mongoose.connect(mongoURI, { dbName: 'stayhealthybeta1'});
+        await mongoose.connect(mongoURI);
         console.info('Connected to Mongo Successfully')
 
         return;
     } catch (error) {
-        console.error(error);
+        console.error('Error connecting to MongoDB:',error);
 
         const nextRetryCount = count + 1;
 
