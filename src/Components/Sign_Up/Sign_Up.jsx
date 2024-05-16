b4537405dbac31662fe39702d57b59e506bb4625
Mostafa Zewail77 <mostafazewail77@gmail.com>
2024-05-16 14:41:37 +0300

    Convert static pages to dynamic React components

---
+++ src/Components/Sign_Up/Sign_Up.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import "./Sign_Up.css"
+
+function Sign_Up() {
+  return (
+    <div className="container">
+        <div className="signup">
+            <div className="signup-text">
+                <h1>Sign Up</h1>
+                <p>Already a member?<span><Link to="/login"> Login</Link> </span> </p>
+            </div>
+
+            <div className="signup-form">
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" id="name" name="name" required className="form-control"placeholder="Enter your name" aria-describedby="helpId"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="phone">Phone</label>
+                        <input type="tel" id="phone" name="phone" required  className="form-control" placeholder="Enter your phone" aria-describedby="helpId"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" required className="form-control" placeholder="Enter your email" aria-describedby="helpId"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name="password" required className="form-control" placeholder="Enter your password" aria-describedby="helpId"/>
+                    </div>
+                    <div className="btn-group">
+                        <button type="submit" className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Submit</button>
+                        <button type="reset" className="btn btn-danger mb-2 waves-effect waves-light">Reset</button>
+                    </div>
+                </form>    
+            </div>
+        </div>    
+    </div>
+  );
+};
+
+export default Sign_Up
