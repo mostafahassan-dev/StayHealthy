d80ee24f95ae297d0a3563e482e7ccc221c848fa
Mostafa Zewail77 <mostafazewail77@gmail.com>
2024-05-21 23:54:53 +0300

    Integrate Instant Consultation component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Navbar from './Components/Navbar/Navbar';
 import LandingPage from './Components/Landing_Page/LandingPage';
 import Login from './Components/Login/Login';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
               <Route path="/" element={<LandingPage/>}/>
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Sign_Up/>}/>
+              <Route path='/instant-consultation' element={<InstantConsultation/>}/>
             </Routes>
             
               
---
+++ src/Components/InstantConsultationBooking/FindDoctorSearchIC/FindDoctorSearchIC.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import './FindDoctorSearchIC.css';
+import { useNavigate, Navigate } from 'react-router-dom';
+
+
+const initSpeciality = [
+    'Dentist', 'Gynecologist/obstetrician', 'General Physician', 'Dermatologist', 'Ear-nose-throat (ent) Specialist', 'Homeopath', 'Ayurveda'
+]
+
+const FindDoctorSearchIC = () => {
+    const [doctorResultHidden, setDoctorResultHidden] = useState(true);
+    const [searchDoctor, setSearchDoctor] = useState('');
+    const [specialities, setSpecialities] = useState(initSpeciality);
+
+    const navigate = useNavigate();
+
+    const handleDoctorSelect = (speciality) => {
+        setSearchDoctor(speciality);
+        setDoctorResultHidden(true);
+        navigate(`/instant-consultation?speciality=${speciality}`);
+        window.location.reload();
+    }
+    return (
+        <div className='finddoctor'>
+            <center>
+                <h1>Find a doctor and Consult instantly</h1>
+                <div>
+                    <i style={{color:'#000000',fontSize:'20rem'}} className="fa fa-user-md"></i>
+                </div>
+                    <div className="home-search-container" >
+
+                        <div className="doctor-search-box">
+                        {/* <p>Perform a search to see the results.</p> */}
+                            <input 
+                                type="text" 
+                                className="search-doctor-input-box" 
+                                placeholder="Search doctors, clinics, hospitals, etc." 
+                                onFocus={() => setDoctorResultHidden(false)} 
+                                onBlur={() => setDoctorResultHidden(true)} 
+                                value={searchDoctor} 
+                                onChange={(e) => setSearchDoctor(e.target.value)} />
+
+                            <div className="findiconimg">
+                                <i className="fa fa-search"></i>
+                            </div>
+                            <div className="search-doctor-input-results" hidden={doctorResultHidden}>
+                                {
+                                    specialities.map(speciality =>
+                                     <div className="search-doctor-result-item" key={speciality} onMouseDown={() => handleDoctorSelect(speciality)}  >
+                                        <i className="fa fa-search"></i>
+                                        <span>{speciality}</span>
+                                        <span>SPECIALITY</span>
+                                    </div>)
+                                }
+                            </div>
+                        </div>
+                    </div>
+            </center>
+        </div>
+    )
+}
+
+export default FindDoctorSearchIC
