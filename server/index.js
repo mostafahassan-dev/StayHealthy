475b1eb6cb6922852cc8e88a0eba72a481d0a8f0
Mostafa Hassan <mostafazewail77@gmail.com>
2024-08-17 14:31:23 +0300

    Add production build process

--- server/index.js
+++ server/index.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const http = require('http');
 const connectToMongo = require('./db');
 const app = express();
-
-
-app.set('view engine','ejs')
-app.use(express.static('public'))
-
+const path = require('path');
 const PORT = process.env.PORT || 8181;
-
 
 // Middleware
 app.use(express.json());
@@ -21,13 +15,15 @@ connectToMongo();
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Catch-all route to serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
-
-  // Start the server
+// Start the server
 app.listen(PORT, () => {
-console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
